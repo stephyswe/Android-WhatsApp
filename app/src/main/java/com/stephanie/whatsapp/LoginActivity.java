b70644cff2c581f1bec35e33ad217f72b87d5401
Stephanie H <stephaniiehallberg@gmail.com>
2018-09-19 16:39:45 +0200

    Initialize Login and Registration

--- app/src/main/java/com/stephanie/whatsapp/LoginActivity.java
+++ app/src/main/java/com/stephanie/whatsapp/LoginActivity.java
@@ -3,18 +3,44 @@ package com.stephanie.whatsapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button LoginButton, PhoneLoginButton;
+    private EditText UserEMail, UserPassword;
+    private TextView NeedNewAccountLink, ForgetPasswordLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initializeFields();
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToRegisterActivity();
+            }
+        });
     }
+
+    private void initializeFields() {
+        LoginButton = findViewById(R.id.login_button);
+        PhoneLoginButton = findViewById(R.id.phone_login_button);
+        UserEMail = findViewById(R.id.login_email);
+        UserPassword = findViewById(R.id.login_password);
+        NeedNewAccountLink = findViewById(R.id.need_new_account_link);
+        ForgetPasswordLink = findViewById(R.id.forget_password_link);
+    }
+
 
     @Override
     protected void onStart() {
@@ -29,5 +55,10 @@ public class LoginActivity extends AppCompatActivity {
     private void SendUserToMainActivity() {
     Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
     startActivity(mainIntent);
+    }
+
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
--- app/src/main/java/com/stephanie/whatsapp/RegisterActivity.java
+++ app/src/main/java/com/stephanie/whatsapp/RegisterActivity.java
@@ -1,13 +1,44 @@
 package com.stephanie.whatsapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button CreateAccountButton;
+    private EditText UserEMail, UserPassword;
+    private TextView AlreadyHaveAccountLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initializeFields();
+
+        AlreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToLoginActivity();
+
+            }
+        });
+    }
+
+    private void initializeFields() {
+        CreateAccountButton = findViewById(R.id.register_button);
+        UserEMail = findViewById(R.id.register_email);
+        UserPassword = findViewById(R.id.register_password);
+        AlreadyHaveAccountLink = findViewById(R.id.already_have_account_link);
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }

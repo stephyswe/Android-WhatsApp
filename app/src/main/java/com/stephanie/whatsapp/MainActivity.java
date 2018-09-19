7762678c1566ab739a44c4fb5e94a0c11a2e0d2f
Stephanie H <stephaniiehallberg@gmail.com>
2018-09-19 17:36:41 +0200

    Drop Down List and SignOut

--- app/src/main/java/com/stephanie/whatsapp/MainActivity.java
+++ app/src/main/java/com/stephanie/whatsapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -55,4 +57,29 @@ public class MainActivity extends AppCompatActivity {
         startActivity(loginIntent);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.main_logout_option) {
+            mAuth.signOut();
+            SendUserToLoginActivity();
+        }
+        if (item.getItemId() == R.id.main_settings_option) {
+
+        }
+
+        if (item.getItemId() == R.id.main_find_friends_option) {
+
+        }
+
+        return true;
+    }
 }

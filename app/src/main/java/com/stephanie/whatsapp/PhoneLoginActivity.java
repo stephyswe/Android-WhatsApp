38d44a119fcbc0e18ba6b8ec74e6028781cd00ac
stephyswe <stephaniiehallberg@gmail.com>
2018-09-19 21:30:44 +0200

    Phone Number Authentication with Firebase

---
+++ app/src/main/java/com/stephanie/whatsapp/PhoneLoginActivity.java
@@ -0,0 +1,36 @@
+package com.stephanie.whatsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class PhoneLoginActivity extends AppCompatActivity {
+
+    private Button SendVerificationCodeButton, VerifyButton;
+    private EditText InputPhoneNumber, InputVerificationCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_login);
+        
+        SendVerificationCodeButton = findViewById(R.id.send_ver_code_button);
+        VerifyButton = findViewById(R.id.verify_button);
+        InputPhoneNumber = findViewById(R.id.phone_number_input);
+        InputVerificationCode = findViewById(R.id.verification_code_input);
+        
+        SendVerificationCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendVerificationCodeButton.setVisibility(View.INVISIBLE);
+                InputPhoneNumber.setVisibility(View.INVISIBLE);
+
+                VerifyButton.setVisibility(View.VISIBLE);
+                InputVerificationCode.setVisibility(View.VISIBLE);
+            }
+        });
+        
+    }
+}

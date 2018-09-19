2aec1db0e5fbc7ec66bf7dced82cb7b852faf430
stephyswe <stephaniiehallberg@gmail.com>
2018-09-19 23:13:45 +0200

    Find Friends Activity - Toolbar with Back Button

--- app/src/main/java/com/stephanie/whatsapp/MainActivity.java
+++ app/src/main/java/com/stephanie/whatsapp/MainActivity.java
@@ -116,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (item.getItemId() == R.id.main_find_friends_option) {
+            SendUserToFindFriendsActivity();
 
         }
 
@@ -180,5 +181,10 @@ public class MainActivity extends AppCompatActivity {
         settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(settingsIntent);
         finish();
+    }
+
+    private void SendUserToFindFriendsActivity() {
+        Intent findFriendsIntent = new Intent(MainActivity.this, FindFriendsActivity.class);
+        startActivity(findFriendsIntent);
     }
 }

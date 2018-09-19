6f1ff8db26cb43821d7eddc7b191d1f6f18551ed
Stephanie H <stephaniiehallberg@gmail.com>
2018-09-19 14:35:48 +0200

    toolbar with title

--- app/src/main/java/com/stephanie/whatsapp/MainActivity.java
+++ app/src/main/java/com/stephanie/whatsapp/MainActivity.java
@@ -2,12 +2,19 @@ package com.stephanie.whatsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("WhatsApp");
     }
 }

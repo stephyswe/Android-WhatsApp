457f5f2f61d0e4bdd99bf4dd2bfd0283b46e9026
Stephanie H <stephaniiehallberg@gmail.com>
2018-10-01 11:23:46 +0200

    Chat - Display Messages RecyclerView and Adapter

--- app/src/main/java/com/stephanie/whatsapp/ChatActivity.java
+++ app/src/main/java/com/stephanie/whatsapp/ChatActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -17,11 +19,16 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -40,6 +47,11 @@ public class ChatActivity extends AppCompatActivity {
 
     private ImageButton SendMessageButton;
     private EditText MessageInputText;
+
+    private final List<Messages> messagesList = new ArrayList<>();
+    private LinearLayoutManager mLinearLayoutManager;
+    private MessageAdapter mMessageAdapter;
+    private RecyclerView userMessagesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,6 +98,49 @@ public class ChatActivity extends AppCompatActivity {
 
         SendMessageButton = findViewById(R.id.send_message_btn);
         MessageInputText = findViewById(R.id.input_message);
+
+        mMessageAdapter = new MessageAdapter(messagesList);
+        userMessagesList = findViewById(R.id.private_messages_list_of_users);
+
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        userMessagesList.setLayoutManager(mLinearLayoutManager);
+        userMessagesList.setAdapter(mMessageAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        RootRef.child("Messages").child(messageSenderID).child(messageReceiverID)
+                .addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        Messages messages = dataSnapshot.getValue(Messages.class);
+
+                        messagesList.add(messages);
+
+                        mMessageAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     private void SendMessage() {
